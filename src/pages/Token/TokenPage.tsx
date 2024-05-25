df4432655c0fee89676defc64a176b9920c55192
Nouman Mahmood <noumanmhd@outlook.com>
2024-05-25 15:52:32 +0300

    refactor: update Uniswap links to use xsidra swap platform

--- src/pages/Token/TokenPage.tsx
+++ src/pages/Token/TokenPage.tsx
@@ -172,7 +172,7 @@ export default function TokenPage() {
               No pool has been created with this token yet. Create one
               <StyledExternalLink
                 style={{ marginLeft: '4px' }}
-                href={`https://app.uniswap.org/#/add/${formattedAddress}`}
+                href={`https://swap.xsidra.com/?token0=${formattedAddress}`}
               >
                 here.
               </StyledExternalLink>
@@ -234,7 +234,7 @@ export default function TokenPage() {
                   </AutoColumn>
                   {activeNetwork !== SidrachianNetworkInfo ? null : (
                     <RowFixed>
-                      <StyledExternalLink href={`https://app.uniswap.org/#/add/${formattedAddress}`}>
+                      <StyledExternalLink href={`https://swap.xsidra.com/add/?token0=${formattedAddress}`}>
                         <ButtonGray width="170px" mr="12px" height={'100%'} style={{ height: '44px' }}>
                           <RowBetween>
                             <Download size={24} />
@@ -242,7 +242,7 @@ export default function TokenPage() {
                           </RowBetween>
                         </ButtonGray>
                       </StyledExternalLink>
-                      <StyledExternalLink href={`https://app.uniswap.org/#/swap?inputCurrency=${formattedAddress}`}>
+                      <StyledExternalLink href={`https://swap.xsidra.com/?token0=${formattedAddress}`}>
                         <ButtonPrimary width="100px" bgColor={backgroundColor} style={{ height: '44px' }}>
                           Trade
                         </ButtonPrimary>
